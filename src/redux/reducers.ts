4b62025ec5c269491062453f966b6883b6f00c1c
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-08 21:09:26 +0300

    Рефакторинг

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -3,20 +3,18 @@ import _ from 'lodash'
 import { type TodoState } from './todoSlice'
 
 export default {
-  addTodo (state: TodoState, action: PayloadAction<{ title: string }>) {
+  addTodo (state: TodoState, action: PayloadAction<string>) {
     state.todos.push({
       id: new Date().toISOString(),
-      title: action.payload.title,
+      title: action.payload,
       completed: false
     })
   },
-  removeTodo (state: TodoState, action: PayloadAction<{ id: string }>) {
-    state.todos = _.reject(state.todos, { id: action.payload.id })
+  removeTodo (state: TodoState, action: PayloadAction<string>) {
+    state.todos = _.reject(state.todos, { id: action.payload })
   },
-  toggleTodoComplete (state: TodoState, action: PayloadAction<{ id: string }>) {
-    const toggledTodo = _.find(state.todos, { id: action.payload.id })
-    if (toggledTodo !== undefined) {
-      toggledTodo.completed = !toggledTodo.completed
-    }
+  toggleTodoComplete (state: TodoState, action: PayloadAction<string>) {
+    const toggledTodo = _.find(state.todos, { id: action.payload })!
+    toggledTodo.completed = !toggledTodo.completed
   }
 }
